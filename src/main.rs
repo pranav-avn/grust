bfd56c75282838ebfd1eabe0afe659bc44ce6443
Pranav AVN <pranavavn@outlook.com>
2024-02-12 22:34:07 +0530

    Add Multiplat Path Handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf, //multi-plat Path Handling
+}
 fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path provided");
 
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
     println!("pattern: {:?}, path: {:?}", pattern, path);
 }
