adb41b10ae3a4eaec29e274ff77827c447e135c3
Pranav AVN <pranavavn@outlook.com>
2024-02-12 23:45:20 +0530

    source structure

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,7 @@ fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file '{}'", args.path.display()))?;
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    grust::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
     Ok(())
 }
